3cca66e16565e83c86b8c25a381c4984c13beab9
Bui Dinh Nguyen <bdnguyen2k2@gmail.com>
2022-12-10 23:34:14 +0700

    fix DBConnection

--- src/main/java/Database/DBConnection.java
+++ src/main/java/Database/DBConnection.java
@@ -9,12 +9,10 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private String uri = "jdbc:mysql://" + DBConfig.host + ":" + DBConfig.port + "/" + DBConfig.dbname;
-
     public static Jdbi jdbi;
     public DBConnection() {
         if(jdbi == null) {
             MysqlDataSource dataSource = new MysqlDataSource();
-
             dataSource.setURL(uri);
             dataSource.setUser(DBConfig.username);
             dataSource.setPassword(DBConfig.password);
@@ -25,11 +23,8 @@ public class DBConnection {
                 error.printStackTrace();
                 throw new RuntimeException(error);
             }
-
             jdbi = Jdbi.create(dataSource);
-
         }
-
     }
 
 }
