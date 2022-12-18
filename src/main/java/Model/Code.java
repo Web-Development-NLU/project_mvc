5f6af39b69b751ffa4e21d90dd10f11f9bad50bb
Bui Dinh Nguyen <bdnguyen2k2@gmail.com>
2022-12-18 13:21:35 +0700

    fix model codeservice

--- src/main/java/Model/Code.java
+++ src/main/java/Model/Code.java
@@ -13,49 +13,8 @@ public class Code extends BaseModel{
         this.value = value;
         this.categoryId = categoryId;
         this.productId = productId;
-        this.validValue = createValidValue();
     }
-    public Code(int categoryId, String productId) {
-        this.categoryId = categoryId;
-        this.productId = productId;
-        this.validValue = createValidValue();
-    }
-    public Code(){
-        this.validValue = createValidValue();
-    }
-
-    public ArrayList<Integer> createValidValue(){
-        ArrayList<Integer> result = new ArrayList<>();
-        for(int i =0; i< 26;i++){
-            result.add(65 + i);
-            result.add(97 + i);
-        }
-        return result;
-    }
-
-    public String createValue(){
-        String result ="";
-        Random random = new Random();
-        char charCode;
-        int intCode;
-        while(result.length() < 6){
-            intCode = validValue.get(random.nextInt(validValue.size()));
-            if(validValue.contains(intCode)){
-                charCode =(char) intCode;
-                result += charCode;
-            }
-        }
-        return result;
-    }
-
-    public boolean checkValidValue(String value){
-        boolean result = true;
-        for(int i=0; i < value.length(); i++){
-            int c = value.charAt(i);
-            if(!validValue.contains(c)) return false;
-        }
-        return result;
-    }
+    public Code(){}
 
     public String getValue() {
         return value;
