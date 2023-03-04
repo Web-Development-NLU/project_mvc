a77ce9ce44d10d21d1a396616dd7944fc6a25bc9
quangtho908 <quangtho23062002@gmail.com>
2023-03-04 12:06:18 +0700

    docker file

--- src/main/java/Database/DBConfig.java
+++ src/main/java/Database/DBConfig.java
@@ -2,8 +2,8 @@ package Database;
 
 public class DBConfig {
     public static String username = "root";
-    public static String password = "root";
-    public static String dbname = "projectfurniture";
-    public static String host = "127.0.0.1";
-    public static String port = "3306";
+    public static String password = "root2306";
+    public static String dbname = "furniture";
+    public static String host = "mysql-109812-0.cloudclusters.net";
+    public static String port = "19995";
 }
