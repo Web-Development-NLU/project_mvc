4f81638d41038856bcc06e56c63fc9ed3abad801
20130401@st.hcmuaf.edu.vn <20130401@st.hcmuaf.edu.vn>
2023-04-25 00:10:57 +0700

    fix regex logic validate password when sign up

--- src/main/java/Utils/Utils.java
+++ src/main/java/Utils/Utils.java
@@ -1,11 +1,29 @@
 package Utils;
 
-public class Utils {
-    public  boolean handleCheckPasswordIsValid(String password) {
-        if (!password.matches("\\.*[aA-zZ0-9].*") || !password.matches("\\.*\\w*[A-Z].*")) {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-            return false;
+public class Utils {
+    public boolean handleCheckPasswordIsValid(String password) {
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{6,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        if (matcher.matches()) {
+            return true;
         }
-        return true;
+        return false;
+
+//        if (!password.matches("\\.*[aA-zZ0-9].*") || !password.matches("\\.*\\w*[A-Z].*") ||
+//                !password.matches("\\.*[a-z].*")) {
+//
+//            return false;
+//        }
+//        return true;
     }
+
+//    public static void main(String[] args) {
+//        Utils u = new Utils();
+//        System.out.print(u.handleCheckPasswordIsValid("123TTTTa"));
+//    }
+
 }
